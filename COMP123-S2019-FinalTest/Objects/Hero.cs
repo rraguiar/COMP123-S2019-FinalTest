711be6ce01ca7ef0d0992ec8756fd5a4ba0ff82e
Rafael Aguiar <rraguiar@yahoo.com>
2019-08-09 09:06:46 -0400

    First Classes - Hero and Power

---
+++ COMP123-S2019-FinalTest/Objects/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * STUDENT NAME: RAFAEL RODRIGUES DE AGUIAR
+ * STUDENT ID: 301041266
+ * DESCRIPTION: This is the Hero Data Container Class
+ * 
+ */ 
+namespace COMP123_S2019_FinalTest.Objects
+{
+    class Hero
+    {
+        //Identity
+        public string HeroName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        //Physical Abilities
+        public string Fighting { get; set; }
+        public string Strength { get; set; }
+        public string Agility { get; set; }
+        public string Endurance { get; set; }
+
+        //Mental Abilities
+        public string Reason { get; set; }
+        public string Intuition { get; set; }
+        public string Psyche { get; set; }
+        public string Popularity { get; set; }
+
+        //Power List
+        List<Power> Powers;
+
+        // Constructor
+        Hero()
+        {
+            //Instantiates an empty Power List
+            Powers = new List<Power>();
+        }
+    }
+}
---
+++ COMP123-S2019-FinalTest/Objects/Power.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * STUDENT NAME: RAFAEL RODRIGUES DE AGUIAR
+ * STUDENT ID: 301041266
+ * DESCRIPTION: This is the Power Class
+ * 
+ */
+namespace COMP123_S2019_FinalTest.Objects
+{
+    
+    class Power
+    {
+        // Public Properties
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+    }
+}
