b77449dfc08b8e15819a1a4bc91eff21fbcedf07
Rafael Aguiar <rraguiar@yahoo.com>
2019-08-09 09:22:41 -0400

    Added secondary abilities to Hero Class

--- COMP123-S2019-FinalTest/Objects/Hero.cs
+++ COMP123-S2019-FinalTest/Objects/Hero.cs
@@ -14,22 +14,135 @@ namespace COMP123_S2019_FinalTest.Objects
 {
     class Hero
     {
+        //PRIVATE INSTANCE VARIABLES
+        private string m_fighting;
+        private string m_strenght;
+        private string m_agility;
+        private string m_endurance;
+        private string m_reason;
+        private string m_intuition;
+        private string m_psyche;
+        private string m_popularity;
+
         //Identity
         public string HeroName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        //PRIMARY ABILITIES
         
         //Physical Abilities
-        public string Fighting { get; set; }
-        public string Strength { get; set; }
-        public string Agility { get; set; }
-        public string Endurance { get; set; }
+        public string Fighting
+        {
+            get
+            {
+                return m_fighting;
+            }
+
+            set
+            {
+                this.m_fighting = value;
+                Health += int.Parse(m_fighting);
+            }
+        }
+
+        public string Strength
+        {
+            get
+            {
+                return m_strenght;
+            }
+
+            set
+            {
+                this.m_strenght = value;
+                Health += int.Parse(m_strenght);
+            }
+        }
+        public string Agility
+        {
+            get
+            {
+                return m_agility;
+            }
+
+            set
+            {
+                this.m_agility = value;
+                Health += int.Parse(m_agility);
+            }
+        }
+        public string Endurance
+        {
+            get
+            {
+                return m_endurance;
+            }
+
+            set
+            {
+                this.m_endurance = value;
+                Health += int.Parse(m_endurance);
+            }
+        }
 
         //Mental Abilities
-        public string Reason { get; set; }
-        public string Intuition { get; set; }
-        public string Psyche { get; set; }
-        public string Popularity { get; set; }
+        public string Reason
+        {
+            get
+            {
+                return m_reason;
+            }
+
+            set
+            {
+                this.m_reason = value;
+                Karma += int.Parse(m_reason);
+            }
+        }
+        public string Intuition
+        {
+            get
+            {
+                return m_intuition;
+            }
+
+            set
+            {
+                this.m_intuition = value;
+                Karma += int.Parse(m_intuition);
+            }
+        }
+        public string Psyche
+        {
+            get
+            {
+                return m_psyche;
+            }
+
+            set
+            {
+                this.m_psyche = value;
+                Karma += int.Parse(m_psyche);
+            }
+        }
+        public string Popularity
+        {
+            get
+            {
+                return m_popularity;
+            }
+
+            set
+            {
+                this.m_popularity = value;
+                Karma += int.Parse(m_popularity);
+            }
+        }
+
+        // SECONDARY ABILITITES
+        public int Health { get; set; }
+        public int Karma { get; set; }
 
         //Power List
         List<Power> Powers;
